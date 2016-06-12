ad480d36dafdc18655ea82154669928837dde8b8
YoannLTH <yoann.lathuiliere@gmail.com>
2016-06-12 12:46:31 +0200

    Code refactoring

--- src/httpserver_model/HTTPServer.java
+++ src/httpserver_model/HTTPServer.java
@@ -17,20 +17,25 @@ import java.util.logging.Logger;
  */
 public class HTTPServer {
 
-    private static final int HTTP_DEFAULT_PORT = 1026;
-    private ServerSocket serverSocket = null;
+    // Informations du serveur HTTP
+    private static final int HTTP_DEFAULT_PORT = 1026; // Port utilisé
+    private ServerSocket serverSocket = null; // Serveur socket
 
+    /**
+     * Fonction qui initilise le serveur en créant un serveur socket sur le port définie et attend les connexions
+     */
     public void initializeServer() throws IOException {
         try {
-            serverSocket = new ServerSocket(HTTP_DEFAULT_PORT);
+            serverSocket = new ServerSocket(HTTP_DEFAULT_PORT); // Création du serveur socket sur le port définie
         } catch (IOException ex) {
             Logger.getLogger(HTTPServer.class.getName()).log(Level.SEVERE, null, ex);
         }
         System.out.println("Listening for connection on port 1026 ....");
+        // Boucle infinie qui attend les connexions
         while (true) {
-            Socket replySocket = serverSocket.accept();            
-            ThreadCommunication tc = new ThreadCommunication(replySocket);
-            tc.start();
+            Socket replySocket = serverSocket.accept(); // Accepte la connexion           
+            ThreadCommunication tc = new ThreadCommunication(replySocket); // Crée un nouveau ThreadCommunication (thread qui gère les communications avec le client)
+            tc.start(); // Démarrage du thread
         }
 
     }
